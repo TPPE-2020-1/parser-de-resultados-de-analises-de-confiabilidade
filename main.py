328d5a459854de022ac7607f4ae6c3712289cfc6
Lucas Dutra <ldutra98@gmail.com>
2020-11-21 16:18:09 -0300

    Add delimiter character method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from Exceptions.ArquivoNaoEncontradoException import ArquivoNaoEncontradoException
+from Exceptions.DelimitadorInvalidoException import DelimitadorInvalidoException
 
 def read_input_file(input_file_name):
     try:
@@ -8,9 +9,17 @@ def read_input_file(input_file_name):
         raise ArquivoNaoEncontradoException(input_file_name)
     return input_file.read()
 
+def delimiter_character(delimiter_symbol):
+    if len(delimiter_symbol) == 1:
+        return delimiter_symbol
+    else:
+        raise DelimitadorInvalidoException(delimiter_symbol)
+
 def main():
-    file_path = input("Insert file path: ") 
+    file_path = input("Insert file path: ")
     file_data = read_input_file(file_path)
+    delimiter_symbol = input("Insert delimiter symbol: ")
+    delimiter_symbol = delimiter_character(delimiter_symbol)
 
 if __name__ == "__main__":
     main()
