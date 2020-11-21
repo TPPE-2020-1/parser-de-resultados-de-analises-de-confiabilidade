b79134e2c528f4639fb1dca89bcda0083c4f20f0
EsioFreitas <esio.gustavo@gmail.com>
2020-11-21 15:45:54 -0300

    Add test_delimiter_input_fileread inout file method

--- test.py
+++ test.py
@@ -2,12 +2,16 @@ import pytest
 from main import read_input_file
 from Exceptions.ArquivoNaoEncontradoException import ArquivoNaoEncontradoException
 
-@pytest.mark.parametrize("input,expected", [('./utils/read_file_de.out', 'Datei gelesen'), ('./utils/read_file_en.out', 'File readed'), ('./utils/read_file_fr.out', 'Lecteur de fichiers'), ('./utils/read_file_pt.out', 'Arquivo lido')])
-def test_read_input_file(input, expected):
-    file = read_input_file(input)
+@pytest.mark.parametrize("input_file,expected", [('./utils/read_file_de.out', 'Datei gelesen'), ('./utils/read_file_en.out', 'File readed'), ('./utils/read_file_fr.out', 'Lecteur de fichiers'), ('./utils/read_file_pt.out', 'Arquivo lido')])
+def test_read_input_file(input_file, expected):
+    file = read_input_file(input_file)
     assert file == expected
 
-@pytest.mark.parametrize("input", [('./utils/read_file_es.out'), ('./test_utils/read_file_en.out')])
-def test_read_input_file_not_found(input):
+@pytest.mark.parametrize("input_file", [('./utils/read_file_es.out'), ('./test_utils/read_file_en.out')])
+def test_read_input_file_not_found(input_file):
     with pytest.raises(ArquivoNaoEncontradoException):
-        assert read_input_file(input)
+        assert read_input_file(input_file)
+
+@pytest.mark.parametrize("input_file,expected", [ (';', ';'), ('\t', '\t'), ('\n', '\n'), ('\r', '\r')])
+def test_delimiter_input_file(input_file, expected):
+    assert delimiter_input_file(input_file) == expected
