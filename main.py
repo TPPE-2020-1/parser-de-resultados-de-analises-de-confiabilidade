cd070c201ea68a051a656cafaf43a16989ad83b1
EsioFreitas <esio.gustavo@gmail.com>
2020-11-21 14:33:06 -0300

    Add read inout file method

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def main():
-    
-    
+from exceptions.ArquivoNaoEncontradoException import ArquivoNaoEncontradoException
 
+def read_input_file(input_file_name):
+    try:
+        input_file = open(input_file_name)
+    except:
+        raise ArquivoNaoEncontradoException(input_file_name)
+    return input_file.read()
+
+def main():
+    file_path = input("Insert file path: ") 
+    file_data = read_input_file(file_path)
 
 if name == "main":
     main()
--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import pytest
 
 @pytest.mark.parametrize("input,expected", [('./utils/read_file_de.out', 'Datei gelesen'), ('./utils/read_file_en.out', 'File readed'), ('./utils/read_file_fr.out', 'Lecteur de fichiers'), ('./utils/read_file_pt.out', 'Arquivo lido')])
 def test_read_input_file(input, expected):
-    file = read_file(input)
+    file = read_input_file(input)
     assert file == expected
 
 @pytest.mark.parametrize("input", [('./utils/read_file_es.out'), ('./test_utils/read_file_en.out')])
 def test_read_input_file_not_found(input, expected):
     with pytest.raises(ArquivoNaoEncontradoException):
-        assert read_file(input)
+        assert read_input_file(input)
