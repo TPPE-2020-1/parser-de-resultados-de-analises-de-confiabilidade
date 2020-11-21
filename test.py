340b603a6680354bc29ca74151c49b43e8860e37
Lucas Dutra <ldutra98@gmail.com>
2020-11-21 14:20:20 -0300

    Create test read_input_file_not_found

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+import pytest
+
 @pytest.mark.parametrize("input,expected", [('./utils/read_file_de.out', 'Datei gelesen'), ('./utils/read_file_en.out', 'File readed'), ('./utils/read_file_fr.out', 'Lecteur de fichiers'), ('./utils/read_file_pt.out', 'Arquivo lido')])
 def test_read_input_file(input, expected):
     file = read_file(input)
     assert file == expected
+
+@pytest.mark.parametrize("input", [('./utils/read_file_es.out'), ('./test_utils/read_file_en.out')])
+def test_read_input_file_not_found(input, expected):
+    with pytest.raises(ArquivoNaoEncontradoException):
+        assert read_file(input)
