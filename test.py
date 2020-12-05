c5cd537399086f53f7058ff1655512910c288ef9
Rogério Júnior <junior.rogerio8@gmail.com>
2020-12-05 11:05:15 -0300

    Change project paradigm to OO
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Esio Freitas <gu.freitas97@gmail.com>

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
 import pytest
-from main import (
-    read_input_file,
-    delimiter_character,
-    output_file,
-    file_format,
-    parse_file_data,
-    write_output_file
-)
+
+from Parser import Parser
 
 from Exceptions.FormatoSaidaArquivoInvalidoException import FormatoSaidaArquivoInvalidoException
 from Exceptions.ArquivoNaoEncontradoException import ArquivoNaoEncontradoException
@@ -23,68 +17,87 @@ from mock.write_output_test import output_file_mock1, output_file_mock2
 
 @pytest.mark.parametrize("input_file,expected", [('./utils/read_file_de.out', 'Datei gelesen'), ('./utils/read_file_en.out', 'File readed'), ('./utils/read_file_fr.out', 'Lecteur de fichiers'), ('./utils/read_file_pt.out', 'Arquivo lido')])
 def test_read_input_file(input_file, expected):
+    parser = Parser()
 
-    file = read_input_file(input_file)
+    file = parser.read_input_file(input_file)
     assert file == expected
 
 
 @pytest.mark.parametrize("input_file", [('./utils/read_file_es.out'), ('./test_utils/read_file_en.out')])
 def test_read_input_file_not_found(input_file):
     with pytest.raises(ArquivoNaoEncontradoException):
-        assert read_input_file(input_file)
+        parser = Parser()
+
+        assert parser.read_input_file(input_file)
 
 
 # Test Definição do delimitador de campo
 
 @pytest.mark.parametrize("input_file,expected", [(';', ';'), ('\t', '\t'), ('\n', '\n'), ('\r', '\r')])
 def test_delimiter_character(input_file, expected):
-    assert delimiter_character(input_file) == expected
+    parser = Parser()
+
+    assert parser.delimiter_character(input_file) == expected
 
 
 @pytest.mark.parametrize("input_file", [('Trennzeichen Beispiel'), ('Exemple de délimiteur'), ('Delimiter Example'), ('Exemplo de delimitador')])
 def test_invalid_delimiter_character(input_file):
     with pytest.raises(DelimitadorInvalidoException):
-        assert delimiter_character(input_file)
+        parser = Parser()
+
+        assert parser.delimiter_character(input_file)
 
 
 # Test Definição do caminho do arquivo de saída.
 
 @pytest.mark.parametrize("directory, file_name, expected", [('utils', 'output_test.out', 'utils/output_testTab.out'), ('./', 'other_output_test.out', './other_output_testTab.out')])
 def test_output_file(directory, file_name, expected):
-    assert output_file(directory, file_name).name == expected
+    parser = Parser()
+
+    assert parser.output_file(directory, file_name).name == expected
 
 
 @pytest.mark.parametrize("directory, file_name", [('unexistent_dir/outputs/', 'output_test'), ('../../', 'other_output_test')])
 def test_invalid_output_file(directory, file_name):
     with pytest.raises(EscritaNaoPermitidaException):
-        assert output_file(directory, file_name)
+        parser = Parser()
+
+        assert parser.output_file(directory, file_name)
 
 
 # Test Definição do formato do arquivo de saída.
 
 @pytest.mark.parametrize("file_format_option, expected", [('colunas', 'colunas'), ('linhas', 'linhas'), ('l', 'l'), ('c', 'c')])
 def test_file_format_option(file_format_option, expected):
-    assert file_format(file_format_option) == expected
+    parser = Parser()
+
+    assert parser.file_format(file_format_option) == expected
 
 
 @pytest.mark.parametrize("file_format_option, expected", [('diagonal', 'diagonal'), ('reverso', 'reverso'), ('d', 'd'), ('r', 'r')])
 def test_invalid_file_format_option(file_format_option, expected):
     with pytest.raises(FormatoSaidaArquivoInvalidoException):
-        assert file_format(file_format_option)
+        parser = Parser()
+
+        assert parser.file_format(file_format_option)
 
 
 # Test Parser de dados
 
 @pytest.mark.parametrize("file_data, expected", [(file_data_mock1['file_data'], file_data_mock1['parsed_data']), (file_data_mock2['file_data'], file_data_mock2['parsed_data'])])
 def test_parse_file_data(file_data, expected):
-    parsed_data = parse_file_data(file_data)
+    parser = Parser()
+
+    parsed_data = parser.parse_file_data(file_data)
     assert parsed_data == expected
 
 
 @pytest.mark.parametrize("file_data", [(invalid_file_data1), (invalid_file_data2)])
 def test_invalid_parse_file_data(file_data):
     with pytest.raises(FormatoArquivoInvalidoException):
-        assert parse_file_data(file_data)
+        parser = Parser()
+
+        assert parser.parse_file_data(file_data)
 
 # Test Escrita do Arquivo
 
@@ -97,8 +110,10 @@ def test_invalid_parse_file_data(file_data):
 def test_write_output_file(parsed_data, delimiter_symbol, output_file, output_format, expected_file):
     outputted_file = open(output_file, "w")
 
-    write_output_file(parsed_data, delimiter_symbol,
-                      outputted_file, output_format)
+    parser = Parser()
+
+    parser.write_output_file(parsed_data, delimiter_symbol,
+                             outputted_file, output_format)
 
     res_file = open(output_file, 'r')
     exp_file = open(expected_file, 'r')
